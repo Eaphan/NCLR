a34333bf9f19decf18e2b4895ee0003f01c128b5
Ivan <183240719@qq.com>
2024-08-24 20:49:18 +0800

    Add downstream datasets.

---
+++ downstream/datasets/__init__.py
@@ -0,0 +1,8 @@
+from .nuscenes_dataset import NuScenes
+from .semantickitti_dataset import SemanticKITTI
+from .kitti3d import KITTI3D
+from .kitti360 import KITTI360
+from .once import ONCE
+
+from .semantic_poss_segcontrast import SemanticPOSS_SegContrast as SemanticPOSS
+# from .livox import LivoxSimu
